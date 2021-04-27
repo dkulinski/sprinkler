9ff15aab6eafcb043d1e530beb796890df2c693f
Dan Kulinski <daniel@kulinski.net>
2021-04-27 04:16:29 +0100

    Change default log level to WARN

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template
 from flask_sprinkler import Sprinkler
 from flask_bootstrap import Bootstrap
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARN)
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -20,7 +20,7 @@ def zone_set():
     zone = request.form['zone']
     time = int(request.form['time'])
     spr.write(f'SPRz{zone}q{time:03}')
-    logging.info(f'Sent SPRz{zone}q{time:03}')
+    logging.debug(f'Sent SPRz{zone}q{time:03}')
     return(render_template('index.html'))
 
 @app.route('/v1/zone', methods=['GET'])
